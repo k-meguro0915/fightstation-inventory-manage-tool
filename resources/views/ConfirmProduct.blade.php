859ac5414791dfe7b5d41bf5146a04718a58a5e9
LAPTOP-LF5QFOAD\work <katsuya.meguro@aion-inc.jp>
2023-01-25 16:15:07 +0900

    商品マスタ　修正

--- resources/views/ConfirmProduct.blade.php
+++ resources/views/ConfirmProduct.blade.php
@@ -7,7 +7,7 @@
   <p>以下の情報で登録・更新します。確認の上、確定ボタンを押してください。</p>
   <form class="flex flex-col flex-wrap" action="/products/commit" method="POST">
     @csrf
-    <input name="product_id" type="text" class="mb-3 rounded"id="productId" @if(!empty($product)) value="{{$product['product_id']}}" @endif @if($is_read==true) readonly="" @endif>
+    <input name="product_id" type="hidden" class="mb-3 rounded"id="productId" @if(!empty($product)) value="{{$product['product_id']}}" @endif>
     @include('components/form/FormProduct',['is_read'=>false])
   </form>
 </div>
