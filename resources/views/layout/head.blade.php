17277e77f8f51d828fc8e936345fae7cbef040f9
LAPTOP-S79JBLEB\katsu <katsuya.meguro@aion-inc.jp>
2023-04-28 15:55:09 +0900

    セキュア設定

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="@yield('description')"><!-- Fonts -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
   <!-- Styles -->
   <style>
     html, body {
